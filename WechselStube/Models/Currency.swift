453b392f24a80eb6bcdd95c2d2e5a0f2e1d7801a
Martin Nguyen <martin@maddin.io>
2021-05-22 15:15:53 +0900

    Added Currency and ExchangeRate data types.

---
+++ WechselStube/Models/Currency.swift
@@ -0,0 +1,38 @@
+//
+//  Currency.swift
+//  WechselStube
+//
+//  Created by Hiki on 2021/05/22.
+//
+
+import Foundation
+
+struct Currency {
+    var code: String
+    var name: String
+}
+
+extension Currency: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(code)
+        hasher.combine(name)
+    }
+}
+
+extension Currency: Identifiable {
+    var id: String {
+        code + name
+    }
+}
+
+extension Currency {
+    
+    static var usd: Self {
+        .init(code: "USD", name: "United States Dollars")
+    }
+    
+    static var jpy: Self {
+        .init(code: "JPY", name: "Japanese Yen")
+    }
+    
+}
---
+++ WechselStube/Models/ExchangeRate.swift
@@ -0,0 +1,27 @@
+//
+//  ExchangeRate.swift
+//  WechselStube
+//
+//  Created by Hiki on 2021/05/22.
+//
+
+import Foundation
+
+struct ExchangeRate {
+    var fromCurrency: Currency
+    var toCurrency: Currency
+    var rate: Double
+}
+
+extension ExchangeRate: Identifiable {
+    var id: String {
+        fromCurrency.code + toCurrency.code
+    }
+}
+
+extension ExchangeRate {
+    static var usdJPY: Self {
+        .init(fromCurrency: .usd,
+              toCurrency: .jpy, rate: 110)
+    }
+}
