6758d8fbe67e8e285671a1d3f018fcaf3a450d43
Martin Nguyen <martin@maddin.io>
2021-05-22 13:43:24 +0900

    Initial Commit

---
+++ WechselStube/WechselStubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WechselStubeApp.swift
+//  WechselStube
+//
+//  Created by Hiki on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WechselStubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
